1ad280be88381119f5aad24cd0c77e2849255088
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-01-20 14:47:56 +0100

    Updated nodes tests

--- src/d4kms_service/node.py
+++ src/d4kms_service/node.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from .neo4j_connection import Neo4jConnection
 
 class Node(BaseModel):
+  uuid: str
 
   @classmethod
   def create(cls, params):
@@ -13,18 +14,18 @@ class Node(BaseModel):
     return result
 
   @classmethod
-  def find(cls, uuid):
+  def find(cls, uuid, raw=False):
     db = Neo4jConnection()
     with db.session() as session:
-      result = session.execute_read(cls._find, cls, uuid)
+      result = session.execute_read(cls._find, cls, uuid, raw)
     db.close()
     return result
 
   @classmethod
-  def exists(cls, key, value):
+  def exists(cls, key, value, raw=False):
     db = Neo4jConnection()
     with db.session() as session:
-      result = session.execute_read(cls._exists, cls, key, value)
+      result = session.execute_read(cls._exists, cls, key, value, raw)
     db.close()
     return result
 
@@ -47,33 +48,7 @@ class Node(BaseModel):
     return dict
 
   @staticmethod
-  def _exists(tx, cls, key, value):
-    where_clause = f'{key}: "{value}"'
-    query = "MATCH (a:%s {%s}) RETURN a" % (cls.__name__, where_clause)
-    result = tx.run(query)
-    for row in result:
-      return cls.wrap(row['a'])
-    return None
-  
-  @staticmethod
-  def _save(tx, self):
-    params = []
-    for param in self.__fields__.keys():
-      params.append(f"a.{param}='{getattr(self, param)}'")
-    params_str = ", ".join(params)
-    query = """
-      MATCH (a:%s {uuid: '%s'})
-      SET %s 
-      RETURN a
-    """ % (self.__class__.__name__, self.uuid, params_str)
-    result = tx.run(query)
-    for row in result:
-      return self.__class__.wrap(row['a'])
-    return None
-  
-  @staticmethod
   def _create(tx, cls, source_params):
-    #print(f"SOURCE_PARAMS: {source_params}")
     params = []
     for param in source_params.keys():
       params.append(f"a.{param}='{source_params[param]}'")
@@ -85,22 +60,42 @@ class Node(BaseModel):
     """ % (cls.__name__, params_str)
     result = tx.run(query, uuid1=str(uuid4()))
     for row in result:
-      dict = {}
-      for items in row['a'].items():
-        dict[items[0]] = items[1]
-      return cls(**dict)
+      return cls.wrap(row['a'])
     return None
 
   @staticmethod
-  def _find(tx, cls, uuid):
-    query = """
-      MATCH (a:%s { uuid: $uuid1 })
-      RETURN a
-    """ % (cls.__name__)
+  def _find(tx, cls, uuid, raw):
+    node_clause = "a" if raw else f"a:{cls.__name__}"
+    query = "MATCH (%s { uuid: $uuid1 }) RETURN a" % (node_clause)
     result = tx.run(query, uuid1=uuid)
     for row in result:
-      dict = {}
-      for items in row['a'].items():
-        dict[items[0]] = items[1]
-      return cls(**dict)
+      if raw:
+        print(f"RAW: {cls.as_dict(row['a'])}")
+      return cls.as_dict(row['a']) if raw else cls.wrap(row['a'])
+    return None
+    
+  @staticmethod
+  def _exists(tx, cls, key, value, raw):
+    node_clause = "a" if raw else f"a:{cls.__name__}"
+    where_clause = f'{key}: "{value}"'
+    query = "MATCH (%s {%s}) RETURN a" % (node_clause, where_clause)
+    result = tx.run(query)
+    for row in result:
+      return cls.as_dict(row['a']) if raw else cls.wrap(row['a'])
+    return None
+  
+  @staticmethod
+  def _save(tx, self):
+    params = []
+    for param in self.model_fields.keys():
+      params.append(f"a.{param}='{getattr(self, param)}'")
+    params_str = ", ".join(params)
+    query = """
+      MATCH (a:%s {uuid: '%s'})
+      SET %s 
+      RETURN a
+    """ % (self.__class__.__name__, self.uuid, params_str)
+    result = tx.run(query)
+    for row in result:
+      return self.__class__.wrap(row['a'])
     return None
--- tests/d4kms_service/test_node.py
+++ tests/d4kms_service/test_node.py
@@ -15,7 +15,6 @@ def test_create():
 def test_find():
   
   class KlassA(Node):
-    uuid: str
     a: str
 
   db = Neo4jConnection()
@@ -30,7 +29,6 @@ def test_find():
 def test_exists():
   
   class KlassA(Node):
-    uuid: str
     a: str
 
   db = Neo4jConnection()
@@ -44,18 +42,48 @@ def test_exists():
 def test_save():
   
   class KlassA(Node):
-    uuid: str
     a: str
+    b: str
+    c: int
 
   db = Neo4jConnection()
   db.clear()
-  db.query("CREATE (a:KlassA) SET a.uuid='1234567890', a.a='test find 2' RETURN a")
+  db.query("CREATE (a:KlassA) SET a.uuid='1234567890', a.a='test find 2', a.b='String B', a.c=10 RETURN a")
   instance = KlassA.find('1234567890')
   assert instance.a == 'test find 2'
+  assert instance.b == 'String B'
+  assert instance.c == 10
   assert db.count() == 1
   instance.a = "Something new"
   instance.save()
   instance = KlassA.find('1234567890')
   assert instance.a == 'Something new'
+  assert instance.b == 'String B'
+  assert instance.c == 10
+  assert db.count() == 1
+  db.close()
+
+def test_raw():
+  
+  class KlassA(Node):
+    a: str
+    b: str
+    c: int
+
+  class KlassB(Node):
+    x: str
+
+  db = Neo4jConnection()
+  db.clear()
+  db.query("CREATE (a:KlassA) SET a.uuid='1234567890', a.a='test find 2', a.b='String B', a.c=10 RETURN a")
+  instance = KlassB.find('1234567890', True)
+  assert instance['a'] == 'test find 2'
+  assert instance['b'] == 'String B'
+  assert instance['c'] == 10
+  assert db.count() == 1
+  instance = KlassA.exists('a', 'test find 2', True)
+  assert instance['a'] == 'test find 2'
+  assert instance['b'] == 'String B'
+  assert instance['c'] == 10
   assert db.count() == 1
   db.close()
