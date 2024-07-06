5eb55941105781101adf2e1c1e321e71588c6d10
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-07-06 12:20:26 +0200

    Rename class

--- src/d4kms_generic/auth0_service.py
+++ src/d4kms_generic/auth0_service.py
@@ -4,7 +4,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from d4kms_generic.service_environment import ServiceEnvironment
 from urllib.parse import quote_plus, urlencode
 
-class Auth0():
+class Auth0Service():
 
   def __init__(self, app: FastAPI) -> None:
     se = ServiceEnvironment()
@@ -38,8 +38,7 @@ class Auth0():
     request.session['access_token'] = token['access_token']
     request.session['id_token'] = token['id_token']
     request.session['userinfo'] = token['userinfo']
-    print(f"USER: {token['userinfo']}")
-
+    
   async def login(self, request: Request):
     return await self.oauth.auth0.authorize_redirect(
       request,
@@ -54,21 +53,19 @@ class Auth0():
     print(f"URL: {url}")
     return url
 
+  def protect_route(self, request: Request, location: str='/login') -> None:
+    """
+    This Dependency protects an endpoint and it can only be accessed if the user has an active session
+    """
+    if not 'id_token' in request.session:  
+      # it could be userinfo instead of id_token
+      # this will redirect people to the login after if they are not logged in
+      raise HTTPException(
+        status_code=status.HTTP_307_TEMPORARY_REDIRECT, 
+        detail="Not authorized",
+        headers={"Location": location}
+      )
 
   def _get_abs_path(self, route: str):
     app_domain = "http://localhost:8000"
     return f"{app_domain}{self.app.url_path_for(route)}"
-
-def protect_endpoint(request: Request) -> None:
-  """
-  This Dependency protects an endpoint and it can only be accessed if the user has an active session
-  """
-  if not 'id_token' in request.session:  
-    # it could be userinfo instead of id_token
-    # this will redirect people to the login after if they are not logged in
-    raise HTTPException(
-      status_code=status.HTTP_307_TEMPORARY_REDIRECT, 
-      detail="Not authorized",
-      headers={"Location": "/login"}
-    )
-
