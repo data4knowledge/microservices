b31a985f39f76c1461833342931174dcb29cca6b
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-11-09 15:49:45 +0100

    Add tests

--- src/d4kms_generic/auth0_management.py
+++ src/d4kms_generic/auth0_management.py
@@ -10,7 +10,7 @@ class Auth0Management():
     self._domain = se.get('AUTH0_DOMAIN')
     self._client_id = se.get('AUTH0_MNGT_CLIENT_ID')
     self._client_secret = se.get('AUTH0_MNGT_CLIENT_SECRET')
-    get_token = GetToken(self._domain, self._client_id, client_secret=self._client_secret)
+    get_token = GetToken(self._domain, self._client_id, self._client_secret)
     token = get_token.client_credentials(f'https://{self._domain}/api/v2/')
     self._auth0 = Auth0(self._domain, token['access_token'])
 
